227755d39dc01e0bf52be302d530e4395a7b4e4a
Todd Flanders <todd.a.flanders@accenture.com>
2021-09-21 20:24:41 -0400

    initial commit

---
+++ BloomFilterClean/BloomFilter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+
+namespace BloomFilterClean
+{
+    public class BloomFilter
+    {
+        private readonly HashFunction _getHashSecondary;
+        private readonly BitArray _hashBits;
+        private readonly int _hashFunctionCount;
+
+        public BloomFilter()
+        {
+            _getHashSecondary = HashString;
+            _hashBits = new BitArray(1000000);
+            _hashFunctionCount = 3;
+        }
+        
+        public void Add(string item)
+        {
+            int primaryHash = item.GetHashCode();
+            int secondaryHash = this._getHashSecondary(item);
+            for (int i = 0; i < this._hashFunctionCount; i++)
+            {
+                int hash = this.ComputeHash(primaryHash, secondaryHash, i);
+                this._hashBits[hash] = true;
+            }
+        }
+        
+        public bool Contains(string item)
+        {
+            int primaryHash = item.GetHashCode();
+            int secondaryHash = this._getHashSecondary(item);
+            for (int i = 0; i < this._hashFunctionCount; i++)
+            {
+                int hash = this.ComputeHash(primaryHash, secondaryHash, i);
+                if (this._hashBits[hash] == false)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        
+        /// <summary>
+        /// Performs Dillinger and Manolios double hashing. 
+        /// </summary>
+        private int ComputeHash(int primaryHash, int secondaryHash, int i)
+        {
+            int resultingHash = (primaryHash + (i * secondaryHash)) % this._hashBits.Count;
+            return Math.Abs((int)resultingHash);
+        }
+        
+        /// <summary>
+        /// Hashes a string using Bob Jenkin's "One At A Time" method from Dr. Dobbs (http://burtleburtle.net/bob/hash/doobs.html).
+        /// Runtime is suggested to be 9x+9, where x = input.Length. 
+        private static int HashString(string s)
+        {
+            int hash = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                hash += s[i];
+                hash += (hash << 10);
+                hash ^= (hash >> 6);
+            }
+
+            hash += (hash << 3);
+            hash ^= (hash >> 11);
+            hash += (hash << 15);
+            return hash;
+        }
+        
+        public delegate int HashFunction(string input);        
+        
+    }
+    
+    
+}
---
+++ BloomFilterDirty/BallOfMud.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+
+namespace BloomFilterDirty
+{
+    public class BallOfMud
+    {
+        private readonly HashFunction _getHashSecondary;
+        private readonly BitArray _hashBits;
+
+        public BallOfMud()
+        {
+            _getHashSecondary = HashString;
+            _hashBits = new BitArray(1000000);
+        }
+        
+        public void Add(string item)
+        {
+            int primaryHash = item.GetHashCode();
+            int secondaryHash = this._getHashSecondary(item);
+            int h1 = primaryHash % this._hashBits.Count;
+            int h2 = (primaryHash +  secondaryHash) % this._hashBits.Count;
+            int h3 = (primaryHash + (2 * secondaryHash)) % this._hashBits.Count;
+            _hashBits[Math.Abs((int)h1)] = true;
+            _hashBits[Math.Abs((int)h2)] = true;
+            _hashBits[Math.Abs((int)h3)] = true;
+        }
+
+        public bool Contains(string item)
+        {
+            int primaryHash = item.GetHashCode();
+            int secondaryHash = this._getHashSecondary(item);
+            int h1 = primaryHash % this._hashBits.Count;
+            int h2 = (primaryHash +  secondaryHash) % this._hashBits.Count;
+            int h3 = (primaryHash + (2 * secondaryHash)) % this._hashBits.Count;
+
+            if (!_hashBits[Math.Abs((int)h1)]) return false;
+            if (!_hashBits[Math.Abs((int)h2)]) return false;
+            if (!_hashBits[Math.Abs((int)h3)]) return false;
+            
+            return true;
+        }
+        
+        public delegate int HashFunction(string input);        
+        
+        private static int HashString(string s)
+        {
+            int hash = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                hash += s[i];
+                hash += (hash << 10);
+                hash ^= (hash >> 6);
+            }
+
+            hash += (hash << 3);
+            hash ^= (hash >> 11);
+            hash += (hash << 15);
+            return hash;
+        }
+        
+    }
+}
